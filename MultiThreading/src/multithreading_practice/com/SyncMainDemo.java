1eec6b10f8646e8d1517437ccb36e20b2a2ad88b
Jyoti Bajaj <aashi@Jyotis-MacBook-Pro.local>
2015-07-28 15:27:47 -0700

    added example of synchronization demo

---
+++ MultiThreading/src/multithreading_practice/com/SyncMainDemo.java
@@ -0,0 +1,39 @@
+package multithreading_practice.com;
+
+public class SyncMainDemo {
+	public static void main(String[] args){
+	SynchronizedDemo threadOne  = new SynchronizedDemo("Thread_One");
+//	NonSynchronized threadTwo  = new NonSynchronized("Thread_Two");
+//	NonSynchronized threadThree  = new NonSynchronized("Thread_Three");
+	// so here we are using three threads on same object of Runnable, hence once a lock is placed on an object .
+	//Other thread wont be able to access it.  
+	Thread thread1 = new Thread(threadOne);
+	Thread thread2 = new Thread(threadOne);
+	Thread thread3 = new Thread(threadOne);
+
+	thread1.start();
+	thread2.start(); 
+	thread3.start(); 
+	//output if synchronized block is not used.
+/*	count number onThread_One 1
+//	count number onThread_One 1
+//	count number onThread_One 1
+//	count number onThread_One 2
+//	count number onThread_One 2
+//	count number onThread_One 2
+//	count number onThread_One 3
+//	count number onThread_One 3
+//	count number onThread_One 3
+//	count number onThread_One 4
+//	count number onThread_One 4
+//	count number onThread_One 4
+//	count number onThread_One 5
+//	count value is:10
+//	count number onThread_One 5
+//	count value is:12
+//	count number onThread_One 5
+//	count value is:12 */
+
+
+	}
+}
---
+++ MultiThreading/src/multithreading_practice/com/SynchronizedDemo.java
@@ -0,0 +1,41 @@
+package multithreading_practice.com;
+
+public class SynchronizedDemo implements Runnable {
+	Thread thread;
+	String threadName;
+	int count;
+	// private so that the object can't be modified from outside.
+	private Object mutex = new Object();
+
+	public SynchronizedDemo(String threadName) {
+		this.threadName = threadName;
+	}
+
+	public void increementMethod() {
+		for (int i = 1; i <= 5; i++) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				System.out.println("thread interrupted");
+			}
+			count++;
+			System.out.println("count number on " + this.threadName + " " + i);
+		}
+		System.out.println("count value is:" + count);
+
+	}
+
+	/*
+	 * synchronized block making sure that method is accessed by one thread at a
+	 * time.hence avoiding discrepancies.
+	 */
+	@Override
+	public void run() {
+		// synchronized block
+		synchronized (mutex) {
+			increementMethod();
+
+		}
+	}
+
+}
